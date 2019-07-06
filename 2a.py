c5eed8ad7274c8040cd61d6acb3c01664e25bf63
Sakthiumamaheshwari97 <51943276+Sakthiumamaheshwari97@users.noreply.github.com>
2019-07-06 06:05:43 -0700

    Create 2a.py

---
+++ 2a.py
@@ -0,0 +1,6 @@
+given=input()
+jug=['Sunday','sunday','Saturday','saturday','Monday','Tuesday','Wednesday','Thursday','Friday']
+if(given==(jug[0] or jug[1]) or jug[2] or jug[3]):
+    print("yes")
+else:
+    print("no")
