32dde3b7d9fc3254e09b18b1010f536db493efac
Sakthiumamaheshwari97 <51943276+Sakthiumamaheshwari97@users.noreply.github.com>
2019-07-09 15:19:37 +0530

    Update 2b.py

--- 2b.py
+++ 2b.py
@@ -1,10 +1,6 @@
 n,k=map(int,input().split())
 h=list(map(int,input().split()))
-p=[]
-for i in h[-1:]:
-    if(k>0):
-        p.append(i)
-        h=-1
-print(p)
-   
+for i in range(0,k):
+    h=[h[-1]]+h[:-1]
+print(*h,end=' ')
     
