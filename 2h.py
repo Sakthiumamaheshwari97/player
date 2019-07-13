c5f3d8c3763681c22b1505403b32ccccdaf16445
Sakthiumamaheshwari97 <51943276+Sakthiumamaheshwari97@users.noreply.github.com>
2019-07-13 14:02:01 +0530

    Update 2h.py

--- 2h.py
+++ 2h.py
@@ -1,7 +1,8 @@
-node=int(raw_input())
-c=0
-for i in range(0,node):
-    ss=raw_input()
-    if(sorted(ss)==sorted("kabali")):
-        c=c+1
-print(c)
+xx=int(input())
+ss="kabali"
+count=0
+for i in range(0,xx,1):
+    ss1=input()
+    if(sorted(ss)==sorted(ss1)):
+       count=count+1
+print(count)
